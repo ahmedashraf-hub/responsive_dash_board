25393aa3c79354101e32fe5b3a7b9b170696fe64
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2025-10-14 00:11:48 +0300

    Add adaptive layout widget.

---
+++ lib/views/widgets/dash_board_view_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/views/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dash_board/views/widgets/dash_board_desktop_layout.dart';
+
+class DashBoardViewBody extends StatelessWidget {
+  const DashBoardViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveLayout(
+      mobileLayout: (context) => const SizedBox(),
+      tabletLayout: (context) => const SizedBox(),
+      desktopLayout: (context) => const DashBoardDesktopLayout(),
+    );
+  }
+}
