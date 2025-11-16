622d966035249344d1c4831e7c3e75555bdb2312
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2025-11-16 03:17:49 +0200

    Remove dashBoardViewBody and edit format.

--- lib/views/dash_board_view.dart
+++ lib/views/dash_board_view.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/widgets/dash_board_view_body.dart';
+import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dash_board/widgets/dash_board_desktop_layout.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: DashBoardViewBody());
+    return Scaffold(
+      body: AdaptiveLayoutWidget(
+        mobileLayout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const DashBoardDesktopLayout(),
+      ),
+    );
   }
 }
