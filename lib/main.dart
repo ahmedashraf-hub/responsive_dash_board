23e4334beed926a1b188c004950e3a3d9496741d
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2025-12-26 13:35:22 +0200

    Using device_preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/dash_board_view.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const ResponsiveDashBoard());
+  runApp(
+    DevicePreview(enabled: true, builder: (context) => ResponsiveDashBoard()),
+  );
 }
 
 class ResponsiveDashBoard extends StatelessWidget {
@@ -11,6 +14,8 @@ class ResponsiveDashBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       home: const DashBoardView(),
     );
